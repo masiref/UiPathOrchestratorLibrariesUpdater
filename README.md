00b66d89c27df7e5f8e79ed0ed47cdde40ab3b8c
Masiré Fofana <masire.fofana@natixis.com>
2020-12-15 00:17:29 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# What is the goal of this PowerShell script?
+
+When using on-premise version of UiPath Orchestrator, libraries used by robots are stored on UiPath Orchestrator and need to be updated manually.
+This script helps to automatically get your UiPath Orchestrator libraries up-to-date, so that when you're upgrading dependencies of projects in UiPath Studio and publishing them to UiPath Orchestrator, latest versions of activities are available for UiPath Robots.
+
+# Configuration
+
+The configuration is handled in `config.json` file. Make a copy of `config.example.json` file and name it `config.json`.
+
+First, you need to specify information about "Reference UiPath Orchestrator". It will be defined as reference, meaning that downloaded libraries will have versions greater than those available in this UiPath Orchestrator.
+Next, you must specify information about "Target UiPath Orchestrator(s)". They will be defined as targets, meaning that downloaded libraries will be uploaded to these UiPath Orchestrators.
+Eventually, you have to define NuGet feeds. They will be searched for new versions of libraries. NuGet feeds are scanned in the order they are defined, it means that if a library is not found in a feed, next one will be searched for.
+
+Note: `config.json`file must be valid against `config-schema.json` schema.
+
+# Execution
+
+After filling `config.json` file, just start UpdateLibraries.ps1 script from a PowerShell command prompt.
+
+`PS C:\Path\To\Script> .\UpdateLibraries.ps1`
+
+# Logs
+
+At each execution, a log file is created in `logs` folder. The file should be checked to point out which libraries were not downloaded successfully.
+
+# Compatibility
+
+The script has been developed under PowerShell 5.1.17763.1490 in a Windows 10 environment, so it should work with above versions of PowerShell.
+The tool has been test on UiPath Orchestrator 2019.10 on-premise version.
+
+# Support
+
+Github: [https://github.com/masiref/UiPathOrchestratorLibrariesUpdater](https://github.com/masiref/UiPathOrchestratorLibrariesUpdater)
+Email: masire.fofana@natixis.com
---
+++ UpdateLibraries.ps1
@@ -0,0 +1,262 @@
+﻿<#
+.DESCRIPTION
+    This script is intented to get newer version of libraries in orchestrator
+    It should be scheduled in order to regularly check if newer version of existing libraries are available in configured NuGet feeds
+.NOTES
+    Script created on 2020/12 by Masire FOFANA (masire.fofana@natixis.com)
+#>
+
+$Config = Get-Content -Raw -Path config.json | ConvertFrom-Json
+
+$ExportedCSVFilesFolder = '.\csv'
+$LogsFolder = '.\logs'
+$DownloadedPackagesFolder = '.\packages'
+
+. .\Function-Write-Log.ps1
+
+# creating logs folder if not exists
+New-Item -ItemType Directory -Force -Path $LogsFolder | Out-Null
+
+# creating csv files folder if not exists
+New-Item -ItemType Directory -Force -Path $ExportedCSVFilesFolder | Out-Null
+
+# creating downloaded packages folder if not exists
+New-Item -ItemType Directory -Force -Path $DownloadedPackagesFolder | Out-Null
+
+# generating log & csv file names
+$FormattedDate = Get-Date -Format "yyyyMMdd-HHmmss"
+$LogFile = "$LogsFolder\$FormattedDate.log"
+$CSVFile = "$ExportedCSVFilesFolder\$FormattedDate.csv"
+
+# creating header in csv file
+"Library,Version" | Add-Content -Path $CSVFile
+
+# UiPath API request global variables definition
+$UiPathAPIRequestGlobalVariables = @{
+    'Headers' = @{
+        'Accept' = 'application/json'
+    }
+    'EndPoints' = @{
+        'Authentication' = 'api/Account/Authenticate'
+        'GetLibraries' = 'odata/Libraries'
+        'GetLibraryVersions' = "odata/Libraries/UiPath.Server.Configuration.OData.GetVersions(packageId='{0}')"
+        'UploadLibrary' = 'odata/Libraries/UiPath.Server.Configuration.OData.UploadPackage'
+    }
+}
+
+# UiPath authentication model
+$LoginModel = @{
+    'tenancyName' = $Config.ReferenceOrchestrator.Tenant
+    'usernameOrEmailAddress' = $Config.ReferenceOrchestrator.Username
+    'password' = $Config.ReferenceOrchestrator.Password
+}
+
+# base URI construction
+$BaseURI = $Config.ReferenceOrchestrator.URL
+If (-Not $BaseURI.EndsWith('/')) {
+	$BaseURI = $BaseURI + '/';
+}
+
+# authenticate to reference orchestrator
+Try {
+    # building URI
+    $URI = $BaseURI + $UiPathAPIRequestGlobalVariables.EndPoints.Authentication
+
+    Write-Log -Message "Authenticating to reference orchestrator" -Path $LogFile -Level Info
+
+    $Headers = $UiPathAPIRequestGlobalVariables.Headers.Clone()
+	$Response = Invoke-RestMethod -Method Post -Uri $URI -Headers $Headers -Body $LoginModel
+
+    Write-Log -Message "Successfully authenticated to $URI on tenant $($Config.ReferenceOrchestrator.Tenant) with user $($Config.ReferenceOrchestrator.Username)"
+
+} Catch {
+    $ErrorMessage = "Unable to communicate with $URI => $_"
+    Write-Log -Message $ErrorMessage -Path $LogFile -Level Error
+	Throw $ErrorMessage
+}
+
+# retrieve all deployed libraries & versions from reference orchestrator in a CSV file
+If ($Response.Success) {
+    # adding token to headers for further requests
+    $Headers.Add('Authorization', "Bearer $($Response.result)")
+    
+    Try {
+        # building URI
+        $URI = $BaseURI + $UiPathAPIRequestGlobalVariables.EndPoints.GetLibraries
+
+        # Write-Log -Message "Retrieving all deployed libraries from reference orchestrator" -Path $LogFile -Level Info
+        # retrieve all deployed libraries
+        $Response = Invoke-RestMethod -Method Get -Uri $URI -Headers $Headers
+        $Libraries = $Response.value
+        $LibrariesCount = $Libraries.Count
+
+        Write-Log -Message "Successfully retrieved $LibrariesCount library(ies)"
+
+        # retrieve all deployed versions of libraries & write in CSV file
+        $CurrentLibraryIndex = 1
+        Foreach ($Library in $Libraries) {
+            # building URI
+            $URI = $BaseURI + ($UiPathAPIRequestGlobalVariables.EndPoints.GetLibraryVersions -f $Library.Id)
+            # Write-Log -Message "Retrieving all deployed versions for library $($Library.Id)" -Path $LogFile -Level Info
+            # retrieve all deployed versions
+            $Response = Invoke-RestMethod -Method Get -Uri $URI -Headers $Headers
+
+            # Write-Log -Message "Successfully retrieved $($Response.value.Count) version(s)"
+            
+            # Write-Log -Message "Writing all deployed versions to $CSVFile file" -Path $LogFile -Level Info
+            # write all versions in CSV file
+            Foreach ($Version in $Response.value) {
+                "$($Library.Id),$($Version.Version)" | Add-Content -Path $CSVFile
+            }
+
+            Write-Log -Message "$CurrentLibraryIndex / $LibrariesCount - Successfully retrieved $($Response.value.Count) version(s) for library $($Library.Id)"
+            $CurrentLibraryIndex++
+
+        }
+    } Catch {
+        $ErrorMessage = "Unable to retrieve all deployed libraries & versions => $_"
+        Write-Log -Message $ErrorMessage -Path $LogFile -Level Error
+	    Throw $ErrorMessage
+    }
+
+    # get distinct list of libraries from CSV file
+    $DistinctLibraries = Import-Csv -Path $CSVFile | Select Library -Unique
+    $DistinctLibrariesCount = $DistinctLibraries.Count
+
+    Write-Log -Message "Starting download of $DistinctLibrariesCount library(ies)"
+
+    # download all versions of libraries from defined nuget feeds
+    $DistinctLibraryIndex = 1
+    Foreach ($DistinctLibrary in $DistinctLibraries) {
+        # building nuget.exe search expression
+        $Search = "packageId:$($DistinctLibrary.Library)"
+
+        Write-Log -Message "$DistinctLibraryIndex / $DistinctLibrariesCount - Downloading $($DistinctLibrary.Library) library" -Level Info
+
+        # loop through NuGet feeds
+        Foreach ($NuGetFeed in $Config.NuGetFeeds) {
+            $NuGetFeedName = $NuGetFeed.Name
+            $NuGetFeedLocation = $NuGetFeed.Location
+
+            Write-Log -Message "Trying to download from [$NuGetFeedName] NuGet feed" -Level Info
+
+            # search libraries with nuget.exe
+            $LibraryVersions = .\nuget.exe list $Search -AllVersions -Source $NuGetFeedLocation
+            
+            # if current NuGet feed gives results, try to download newer versions
+            If ($LibraryVersions -is [array] -or ($LibraryVersions -is [string] -and $LibraryVersions.StartsWith($DistinctLibrary.Library))) {
+            
+                $HigherAlreadyDeployedVersion = (Import-Csv -Path $CSVFile | Where-Object { $_.Library -eq $DistinctLibrary.Library } | Sort-Object -Property Version -Descending | Select -First 1).Version
+
+                Write-Log -Message "Downloading versions higher than $HigherAlreadyDeployedVersion" -Path $LogFile -Level Info
+
+                Foreach ($LibraryVersion in $LibraryVersions) {
+                    $Library = $LibraryVersion.Split(' ').Item(0)
+                    $Version = $LibraryVersion.Split(' ').Item(1)
+
+                    # download version if greater than higher version already deployed in reference orchestrator
+                    If ([System.Version]$Version -gt [System.Version]$HigherAlreadyDeployedVersion) {
+                        Try {
+                            # Write-Log -Message "Downloading version $Version" -Path $LogFile -Level Info
+                            .\nuget.exe install $Library -Version $Version -OutputDirectory $DownloadedPackagesFolder -Source $NuGetFeedLocation -PackageSaveMode nupkg | Out-Null
+
+                            Write-Log -Message "Version $Version successfully downloaded" -Path $LogFile -Level Info
+
+                        } Catch {
+                            Write-Log -Message "Error while downloading version $Version => $_" -Path $LogFile -Level Error
+                        }
+                    }
+                }
+
+                # download next library versions => exit NuGetFeeds loop
+                Break
+            } Else {
+                Write-Log -Message 'Library not found' -Path $LogFile -Level Warn
+            }
+        }
+
+        $DistinctLibraryIndex++
+    }
+
+    # upload downloaded libraries to target orchestrators
+    Foreach ($TargetOrchestrator in $Config.TargetOrchestrators) {
+        # UiPath authentication model
+        $LoginModel = @{
+            'tenancyName' = $TargetOrchestrator.Tenant
+            'usernameOrEmailAddress' = $TargetOrchestrator.Username
+            'password' = $TargetOrchestrator.Password
+        }
+
+        # base URI construction
+        $BaseURI = $TargetOrchestrator.URL
+        If (-Not $BaseURI.EndsWith('/')) {
+	        $BaseURI = $BaseURI + '/';
+        }
+
+        # authenticate to target orchestrator
+        Try {
+            # building URI
+            $URI = $BaseURI + $UiPathAPIRequestGlobalVariables.EndPoints.Authentication
+
+            Write-Log -Message "Authenticating to $BaseURI orchestrator" -Path $LogFile -Level Info
+            
+            $Headers = $UiPathAPIRequestGlobalVariables.Headers.Clone()
+	        $Response = Invoke-RestMethod -Method Post -Uri $URI -Headers $Headers -Body $LoginModel
+
+            Write-Log -Message "Successfully authenticated to $URI on tenant $($TargetOrchestrator.Tenant) with user $($TargetOrchestrator.Username)"
+
+        } Catch {
+            $ErrorMessage = "Unable to communicate with $URI => $_"
+            Write-Log -Message $ErrorMessage -Path $LogFile -Level Error
+        }
+
+        # upload downloaded libraries
+        If ($Response.Success) {
+            # adding token to headers for further requests
+            $Headers.Add('Authorization', "Bearer $($Response.result)")
+
+            # adding content type key with empty value
+            $Headers.Add('Content-Type', '')
+
+            Get-ChildItem -Path .\packages -Filter *.nupkg -Recurse | % {
+                $Library = [System.IO.Path]::GetFileNameWithoutExtension($_)
+                $LibraryLocation = $_.FullName
+
+                $FileBytes = [System.IO.File]::ReadAllBytes($LibraryLocation);
+                $FileEnc = [System.Text.Encoding]::GetEncoding('ISO-8859-1').GetString($FileBytes);
+                $Boundary = [System.Guid]::NewGuid().ToString(); 
+                $LF = "`r`n";
+
+                $BodyLines = ( 
+                    "--$Boundary",
+                    "Content-Disposition: form-data; name=`"file`"; filename=`"$(Split-Path $LibraryLocation -Leaf)`"",
+                    "Content-Type: application/octet-stream$LF",
+                    $FileEnc,
+                    "--$Boundary--$LF" 
+                ) -join $LF
+                
+                $Headers['Content-Type'] = "multipart/form-data; boundary=`"$Boundary`""
+
+                $URI = $BaseURI + $UiPathAPIRequestGlobalVariables.EndPoints.UploadLibrary
+
+                Try {
+                    $Response = Invoke-RestMethod -Method Post -Uri $URI -Headers $Headers -Body $BodyLines
+
+                    Write-Log -Message "$Library uploaded successfully" -Level Info
+                } Catch {
+                    $ErrorDetails = $_.ErrorDetails.Message | ConvertFrom-Json
+                    If ($ErrorDetails.message -ne 'Package already exists.') {
+                        Write-Log -Message "Unable to upload library $Library => $_" -Path $LogFile -Level Error
+                    } Else {
+                        Write-Log -Message "Library $Library already exists" -Level Warn
+                    }
+                }
+            }
+        }
+    }
+
+} Else {
+    $ErrorMessage = "Unable to authenticate to $URI on tenant $($Config.ReferenceOrchestrator.Tenant) with user $($Config.ReferenceOrchestrator.Username) => $($Response.result)"
+    Write-Log -Message $ErrorMessage -Path $LogFile -Level Error
+	Throw $ErrorMessage
+}
